e415cc197876766b08b07cee0ee72f2a5f098afa
Ethan-Ch1u <92735665+Ethan-Ch1u@users.noreply.github.com>
2023-02-10 10:58:45 -0500

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -31,14 +31,34 @@ function animate() {
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+//Part 2 constructor 
+
 
 function swapPhoto() {
+
+
+	//if the current index is greater than or equal to the length of mImages and an if that statement is true, set the current index to 0
+
+
+	if(mCurrentIndex >= mImages.length){
+	mCurrentIndex = 0;
+}
+if(mCurrentIndex < 0 ){
+	mCurrentIndex = mImages.length - 1;
+}
+//Accesses the 'photo' in the HTML document by creating 3 variables, for location, description, and date
+	
 	//Add code here to access the #slideShow element.
 	document.getElementById('photo').src = mImages[mCurrentIndex].img;
-	var location = document.getElementById('location');
-	location[0].innerHTML = 'location: ' = mImages[mCurrentIndex].location;
-	var description = document.getElementById('description');
-	var date = document.getElementById('date');
+	var loc = document.getElementsByClassName('location');
+	loc[0].innerHTML = 'location: ' + mImages[mCurrentIndex].location;
+	var des = document.getElementsByClassName('description');
+	des[0].innerHTML = 'description: ' + mImages[mCurrentIndex].description;
+	var dt = document.getElementsByClassName('date');
+	dt[0].innerHTML = 'date: ' + mImages[mCurrentIndex].date;
+
+	mLastFrameTime = 0;
+	mCurrentIndex += 1;
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
@@ -55,20 +75,36 @@ var mRequest = new XMLHttpRequest();
 var mImages = [];
 
 // Holds the retrived JSON information
-var mJson;
+let mJson = "";
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
 var mUrl = 'images.json'; 
 
+
+
 function fetchJSON(){
 	mRequest.onreadystatechange = function (){
 		if(this.readyState == 4 && this.status == 200){
-			responseText = JSON.parse(this.responseText);
+			mJson = JSON.parse(mRequest.responseText);
+			iterateJSON(mJson);
 		}
 	}
-	mRequest.open("Get", mUrl, true);
+	mRequest.open("GET", mUrl, true);
 	mRequest.send();
+	
+}
+
+function iterateJSON(){
+	//for loop that access the mImages array by using the variable x as the index
+	
+	for (x = 0; x < mJson.images.length; x++) {
+		mImages[x] = new GalleryImage();
+		mImages[x].location = mJson.images[x].imgLocation;
+		mImages[x].description = mJson.images[x].description;
+		mImages[x].date = mJson.images[x].date;
+		mImages[x].img = mJson.images[x].imgPath;
+	}
 }
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -81,11 +117,34 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 }
 
 $(document).ready( function() {
-	
+
+		//offset the #nextPhoto image so that it is flush with the right side of the #nav div
+
+	$("#nextPhoto").position({
+		my: "right top",
+  		at: "right top",
+  		of: "#nav"
+	});
 	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
+	//$('.details').eq(0).hide();
+	fetchJSON();
+	//Add a click handler to the img.moreIndicator that Add a class attribute value = “rot270” if the element currently has a class value with “rot90”; else remove the “rot270” class and add “rot90”, causing the arrow to animate upside down.
+
+	$( ".moreIndicator" ).click(function() {
+  		if($(this).hasClass("rot90")){
+			$(this).removeClass("rot90")
+			$(this).addClass("rot270")
+			$( ".details" ).slideToggle( "slow")
+		}else{
+			$(this).removeClass("rot270");
+			$(this).addClass("rot90")
+			$( ".details" ).slideToggle( "slow")
+
+		}
+	});
 	
 });
+
 
 window.addEventListener('load', function() {
 	
@@ -93,14 +152,50 @@ window.addEventListener('load', function() {
 
 }, false);
 
+
+
+//Assigns data from JSON file to file that will be used on slideshow
 function GalleryImage() {
-	var location = $(".location");
-	var description = $(".description");
-	var date = $(".description");
-	var img = $("#photo");
+	var location
+	var description
+	var date 
+	var source 
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo
 	//3. the date when the photo was taken
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
 }
+
+
+//Click handlers that goes to the next photo or the previous photo in the array order
+$(document).ready( function() {
+
+	
+	$("#nextPhoto").click(function(){
+		if(mCurrentIndex >= mImages.length){
+			mCurrentIndex = 0;
+		}
+if(mCurrentIndex < 0 ){
+	mCurrentIndex = mImages.length - 1;
+}
+//Accesses the 'photo' in the HTML document by creating 3 variables, for location, description, and date
+	
+	//Add code here to access the #slideShow element.
+	swapPhoto();
+	});
+		$("#prevPhoto").click(function(){
+			mCurrentIndex -= 2;
+		if(mCurrentIndex >= mImages.length){
+			mCurrentIndex = 0;
+		}
+if(mCurrentIndex < 0 ){
+	mCurrentIndex = mImages.length - 1;
+}
+//Accesses the 'photo' in the HTML document by creating 3 variables, for location, description, and date
+	
+	//Add code here to access the #slideShow element.
+	swapPhoto();
+	});
+});
+
